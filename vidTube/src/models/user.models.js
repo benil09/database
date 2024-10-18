d022e2bcbe6aef55a7e635f1c702030e84d29e49
benil09 <benil8235@gmail.com>
2024-10-18 11:50:42 +0530

    hooks and methods in mongoose with jwt tokens

--- vidTube/src/models/user.models.js
+++ vidTube/src/models/user.models.js
@@ -1,5 +1,7 @@
 
 import mongoose,{Schema} from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema=new Schema(
     {
@@ -50,5 +52,47 @@ const userSchema=new Schema(
     },
     {timestamps:true}
 )
+
+
+userSchema.pre("save",async function (next){
+    if(!this.modified("password")) return next()
+    this.password=bcrypt.hash(this.password,10)
+
+
+  
+})
+
+
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    await   bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    // short lived access token
+    jwt.sign({
+       _id: this._id,
+       email:this.email,
+       username:this.username,
+       username:this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+)
+}
+userSchema.methods.isPasswordCorrect = async function (password){
+    await   bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    // short lived access token
+    jwt.sign({
+       _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+)
+}
+
 
 export const User =mongoose.model("User",userSchema)
