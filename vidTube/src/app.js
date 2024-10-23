56fa06ee561083b12015e81f45bc5e38cc9a8831
benil09 <benil8235@gmail.com>
2024-10-23 10:11:04 +0530

    handling mern application

--- vidTube/src/app.js
+++ vidTube/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from 'cors'
+import cookie-parser from "cookie-parser"
 
 const app=express()
 
@@ -16,7 +17,7 @@ app.use(
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static("public"))
-
+app.use((cookieParser()))
 
 //import routes
 
